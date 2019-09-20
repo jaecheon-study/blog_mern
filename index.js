1a1d9d0450f1b1dfe4dd284d65e5427d0d1532f2
jaecheon <kzc0222@gmail.com>
2019-09-20 14:00:31 +0900

    add morgan, body-parer and mongoose (connect)

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ const express = require('express');
 const app = express();
 //process.env.PORT에 설정된 PORT가 있으면 사용 없으면 정의한 3000 사용 
 const PORT = process.env.SERVER_PORT || 3000;
+const mongoose = require('mongoose');
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // 라우터 설정
 const postsRouter = require('./api/route/posts');
 const profilesRouter = require('./api/route/profiles');
 const usersRouter = require('./api/route/users');
+
+mongoose.connect(process.env.MONGO_URL, {dbName: 'blog_mern', useNewUrlParser: true, useCreateIndex: true})
+    .then(() => console.log(`MongoDb Connect...`))
+    .catch(err => console.log(`error: ${err}`));
+
+// morgan 설정
+app.use(morgan('dev'));
+// body-parser 설정
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // 라우터 경로 설정 (첫번 째 아규먼트 경로 주소, 두번 째 아규먼트 라우터 파일 위치)
 app.use('/posts', postsRouter);
